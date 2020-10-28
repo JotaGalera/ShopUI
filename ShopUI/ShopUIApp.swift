e8a4dfcd138c77274f32ccd49ae064b1057a9811
Jota Galera <javigalera94@gmail.com>
2020-10-28 23:00:53 +0100

    Closed #6 create ShopUI project

---
+++ ShopUI/ShopUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopUIApp.swift
+//  ShopUI
+//
+//  Created by Javier Galera Garrido on 28/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct ShopUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
