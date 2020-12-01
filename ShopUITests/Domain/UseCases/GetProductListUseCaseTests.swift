c0afb58acb4da7fa6a7f737607a67f3cf4841dc2
Jota Galera <javigalera94@gmail.com>
2020-12-01 22:20:49 +0100

    Closed #30 Get ProductList tests cover all code

--- ShopUITests/Domain/UseCases/GetProductListUseCaseTests.swift
+++ ShopUITests/Domain/UseCases/GetProductListUseCaseTests.swift
@@ -11,11 +11,13 @@ import XCTest
 
 class GetProductListUseCaseTests: XCTestCase {
     private var sut: GetProductListUseCase?
-    private var productListConverterMock = ProductListConverterMock()
-    private var repositoryMock = APIRepositoryMock()
+    private var productListConverterMock: ProductListConverterMock!
+    private var repositoryMock: APIRepositoryMock!
     
     override func setUp() {
         super.setUp()
+        productListConverterMock = ProductListConverterMock()
+        repositoryMock = APIRepositoryMock()
         setUpMocks()
         sut = GetProductListUseCaseImplementation(repository: repositoryMock, converter: productListConverterMock)
     }
@@ -24,14 +26,36 @@ class GetProductListUseCaseTests: XCTestCase {
         productListConverterMock.convertProductListDataReturnValue = ProductListMock()
     }
     
-    func testThatExpectationIsFulFill_When_ExecuteIsCalled(){
-        let expectation = XCTestExpectation(description: "Completion triggered")
+    func testThatRepositoryMockIsCalled_When_ExecuteIsCalled() {
+        let successExpectation = expectation(description: "success")
+        repositoryMock.getProductListOnSuccessOnFailureClosure = {
+            success , _ in
+            let productListDataMock = [["name":"mock name","brand":"mock brand","price":10,"currency":"€","image":"mock image"]]
+            success(productListDataMock)
+        }
         
         _ = sut?.execute(onSuccess: { productlist in
-            expectation.fulfill()
-        }, onFailure: { _ in
-            expectation.fulfill()
-        })
+            XCTAssertEqual(1, self.repositoryMock.getProductListOnSuccessOnFailureCallsCount)
+            successExpectation.fulfill()
+        }, onFailure: { _ in })
         
+        waitForExpectations(timeout: 10)
+    }
+    
+    func testThatErrorIsReturned_When_ExecuteIsCalled() {
+        let failureExpectation = expectation(description: "failure")
+        repositoryMock.getProductListOnSuccessOnFailureClosure = {
+            _ , failure in
+            let productListDataMock = "Data could not be getted"
+            failure(productListDataMock)
+        }
+        
+        _ = sut?.execute(onSuccess: { _ in },
+                         onFailure: { error in
+                            XCTAssertEqual("Data could not be getted", error)
+                            failureExpectation.fulfill()
+                         })
+        
+        waitForExpectations(timeout: 10)
     }
 }
