81ca0aede6dcff437ef11ad6bc190bb8cb265d57
Jota Galera <javigalera94@gmail.com>
2020-12-11 18:37:50 +0100

    Closed #49 add the new URL to API in order to get the product details

--- ShopUI/Data/DataSource/API/APIData/APIData.swift
+++ ShopUI/Data/DataSource/API/APIData/APIData.swift
@@ -9,4 +9,5 @@ import Foundation
 
 struct APIData {
     static let getProducts = "https://api-shop-ui.herokuapp.com/products"
+    static let getDetails = "https://api-shop-ui.herokuapp.com/products/details/?product_id="
 }
