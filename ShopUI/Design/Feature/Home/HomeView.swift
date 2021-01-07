af4a340bcbd462750c5d64c7da50dc0b0ce99389
Jota Galera <javigalera94@gmail.com>
2021-01-07 21:41:52 +0100

    Improving bottom bar design , first iteration

--- ShopUI/Design/Feature/Home/HomeView.swift
+++ ShopUI/Design/Feature/Home/HomeView.swift
@@ -10,6 +10,7 @@ struct HomeView: View {
     @StateObject var viewRouter: ViewRouter
     @StateObject var productListVM = ProductListViewModelImplementation()
     @StateObject var wishlistVM = WishlistViewModelImplementation()
+    @State var selected = false
     
     var body: some View{
         GeometryReader { geometry in
@@ -18,13 +19,17 @@ struct HomeView: View {
                 switch viewRouter.currentPage {
                 case .home:
                     ProductListView(productListViewModel: productListVM, wishlistViewModel: wishlistVM)
+                        .onTapGesture {
+                            print("hola")
+                        }
                 case .wishlist:
                     WishlistView(wishlistViewModel: wishlistVM)
                 }
                 Spacer()
                 HStack {
-                    TabBarIcon(assignedPage: .home, width: geometry.size.width/5, height: geometry.size.height/28, systemIconName: "homekit", tabName: "Home")
-                    TabBarIcon(assignedPage: .wishlist, width: geometry.size.width/5, height: geometry.size.height/28, systemIconName: "heart", tabName: "Wishlist")
+                    TabBarIcon(selected: $selected, assignedPage: .home, width: geometry.size.width/5, height: geometry.size.height/26, systemIconName: "homekit", tabName: "Home")
+                        
+                    TabBarIcon(selected: $selected, assignedPage: .wishlist, width: geometry.size.width/5, height: geometry.size.height/28, systemIconName: "heart", tabName: "Wishlist")
                 }
                 .frame(width: geometry.size.width, height: geometry.size.height/8)
                 .background(Color("TabBarBackground")
@@ -42,25 +47,30 @@ struct HomeView: View {
 
 struct TabBarIcon: View {
     @EnvironmentObject var viewRouter: ViewRouter
+    @Binding var selected: Bool
+    @State private var scale: CGFloat = 1
     let assignedPage: Page
     
     let width, height: CGFloat
     let systemIconName, tabName: String
     
     var body: some View {
-        VStack{
+        VStack {
             Image(systemName: systemIconName)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
+                .scaleEffect(selected ? 1.2 : 1.0)
+                .animation(Animation.easeInOut(duration: 1))
                 .frame(width: width, height: height)
                 .padding(.top,10)
             Text(tabName)
                 .font(.footnote)
             Spacer()
         }
-        .padding(.horizontal, -4)
         .onTapGesture {
             viewRouter.currentPage = assignedPage
+            selected.toggle()
+            
         }
     }
 }
